17aaa5c6103d64d79feac3b7631f71f862421f69
Jordin Gardner <gard.jordin@gmail.com>
2020-12-28 12:38:03 -0700

    initial commit - moving independent paddles

---
+++ Assets/GenerateCollisionGeometry.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class GenerateCollisionGeometry : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playArea = null;
+    private Sprite pas = null;
+
+    PolygonCollider2D pc;
+
+    void Start()
+    {
+        pc = GetComponent<PolygonCollider2D>();
+        pc.pathCount = 2;
+
+        if (playArea != null) {
+            pas = playArea.GetComponent<SpriteRenderer>().sprite;
+        }
+
+        updateGeometry();
+    }
+
+    void Update()
+    {
+        updateGeometry();
+    }
+
+    private void updateGeometry() {
+        float x = playArea.transform.position.x;
+        float y = playArea.transform.position.y; 
+
+        float w = pas.bounds.extents.x * playArea.transform.localScale.x;
+        float h = pas.bounds.extents.y * playArea.transform.localScale.y;
+
+        Vector2[] playAreaVertices = new Vector2[]
+        {
+            new Vector2(x - w, y + h),
+            new Vector2(x + w, y + h),
+            new Vector2(x + w, y - h),
+            new Vector2(x - w, y - h),
+        };
+
+        pc.SetPath(1, playAreaVertices);
+    }
+}
---
+++ Assets/PaddleController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+
+public class PaddleController : MonoBehaviour
+{
+    [Header("Controls")]
+    [SerializeField]
+    private KeyCode up = KeyCode.W;
+    [SerializeField]
+    private KeyCode down = KeyCode.S;
+
+    [Header("Movement Values")]
+    [SerializeField]
+    private float acceleration = 0.0f;
+    [SerializeField]
+    private float accelerationDirectionChange = 0.0f;
+    [SerializeField]
+    private float maxSpeed = 0.0f;
+
+    private Vector2 movementVector => 
+        new Vector2(0, acceleration);
+
+    private Vector2 directionChangeVector =>
+        new Vector2(0, accelerationDirectionChange);
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        if (Input.GetKey(up) && rb.velocity.y < maxSpeed) {
+            Move(movementVector * Time.deltaTime);
+            if (rb.velocity.y < 0) {
+                Move(directionChangeVector * Time.deltaTime);
+            }
+        }
+        else if (Input.GetKey(down) && rb.velocity.y > -maxSpeed) {
+            Move(-movementVector * Time.deltaTime);
+            if (rb.velocity.y > 0) {
+                Move(-directionChangeVector * Time.deltaTime);
+            }
+        }
+    }
+
+    private void Move(Vector2 vector) {
+        rb.AddForce(
+            vector,
+            ForceMode2D.Impulse
+        );
+    }
+}
